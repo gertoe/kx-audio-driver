ac027c539f03a611f60fef98d8c2f95b3bf9e626
ITzTravelInTime <pietrocaruso97@yahoo.it>
2019-11-14 16:15:28 +0100

    Updated versions and credits

--- h/vers.h
+++ h/vers.h
@@ -9,29 +9,31 @@
 #ifndef VER_H_
 #define VER_H_
 
-#define KX_DRIVER_DATE		03/11/2018
+#define KX_DRIVER_DATE        11/15/2019 //MM/GG/YYYY
 
 
-#define KX_VERSION_DWORD		0x51003553
+#define KX_VERSION_DWORD        0x51003553
 #ifdef KX_DEBUG
- #define KX_DRIVER_VERSION_STR		"5.10.00.3553 - debug"
+ #define KX_DRIVER_VERSION_STR        "5.20.00.0 - debug"
 #else
- #define KX_DRIVER_VERSION_STR		"5.10.00.3553"
+ #define KX_DRIVER_VERSION_STR        "5.20.00.0"
 #endif
-#define KX_DRIVER_VERSION		5,10,0,3553
-#define KX_DRIVER_VERSION_		5.10.0.3553
+#define KX_DRIVER_VERSION        5,20,0,0
+#define KX_DRIVER_VERSION_        5.20.0.0
 #ifdef KX_DEBUG
- #define KX_VERSION_STR			"5, 10, 00, 3553 - debug"
+ #define KX_VERSION_STR            "5, 20, 00, 0 - debug"
 #else
- #define KX_VERSION_STR			"5, 10, 00, 3553"
+ #define KX_VERSION_STR            "5, 20, 00, 0"
 #endif
-#define KX_COPYRIGHT_STR		"Copyright © Eugene Gavrilov, 2001-2018. All rights reserved. Mod By ITzTravelInTime"
-#define KX_COPYRIGHT_STR_R		"Copyright © Eugene Gavrilov, 2001-2018. Mod By ITzTravelInTime"
-#define KX_COPYRIGHT_YEARS		"2001-2018"
-#define KX_COMPANY_NAME			"Eugene Gavrilov, Mod By ITzTravelInTime"
+#define KX_COPYRIGHT_STR        "Copyright © Eugene Gavrilov, 2001-2018. All rights reserved. Mod By Alejandro, ITzTravelInTime"
+
+#define KX_COPYRIGHT_STR_R        "Copyright © Eugene Gavrilov, 2001-2018. Mod By Alejandro, ITzTravelInTime"
+#define KX_COPYRIGHT_YEARS        "2001-2018"
+
+#define KX_COMPANY_NAME            "Eugene Gavrilov, Mod By Alejandro, ITzTravelInTime"
 
 #define KX_AC3_VERSION "8.1.0"
 
-#define KX_ASIO_VERSION		3553
+#define KX_ASIO_VERSION        3553
 
 #endif
